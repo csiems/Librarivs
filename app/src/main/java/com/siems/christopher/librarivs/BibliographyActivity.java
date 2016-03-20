703ba0b945a4d37d8a8b8141f28462cc30f40aee
Christopher Siems <csiems@gmail.com>
2016-03-20 13:11:23 -0700

    display user input on bibliography activity

--- app/src/main/java/com/siems/christopher/librarivs/BibliographyActivity.java
+++ app/src/main/java/com/siems/christopher/librarivs/BibliographyActivity.java
@@ -1,12 +1,17 @@
 package com.siems.christopher.librarivs;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class BibliographyActivity extends AppCompatActivity {
+    @Bind(R.id.bibliographyTextView) TextView bibliographyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,7 @@ public class BibliographyActivity extends AppCompatActivity {
                 .build()
         );
         setContentView(R.layout.activity_bibliography);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String authorFirstName = intent.getStringExtra("authorFirstName");
@@ -28,5 +34,16 @@ public class BibliographyActivity extends AppCompatActivity {
         String publisher = intent.getStringExtra("publisher");
         String bookYear = intent.getStringExtra("bookYear");
         String bookCity = intent.getStringExtra("bookCity");
+
+        Resources res = getResources();
+        String bibliography;
+
+        if (bookVol.equals("")) {
+            bibliography = String.format(res.getString(R.string.bibliographyBookStandard), authorLastName, authorFirstName, bookTitle,                             bookCity, publisher, bookYear);
+        } else {
+            bibliography = String.format(res.getString(R.string.bibliographyBookAdvanced), authorLastName, authorFirstName, bookTitle,                             bookEd, bookVol, bookCity, publisher, bookYear, bookSeries);
+        }
+
+        bibliographyTextView.setText(bibliography);
     }
 }
